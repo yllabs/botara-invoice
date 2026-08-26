385228bdf6e28447834c0a4ae0fd23782fc4d026
noahhyy <126304197+noahhyy@users.noreply.github.com>
2026-08-26 00:42:49 -0400

    Add API for sending PDF invoices via email
    This file implements an API endpoint for sending PDF invoices via email. It includes functions for generating the PDF content, handling email sending through Gmail API, and processing invoice data.

---
+++ api/send-invoice.js
@@ -0,0 +1,144 @@
+const LOGO_JPEG_BASE64 = "/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU3Ojo6Iys/RD84QzQ5OjcBCgoKDQwNGg8PGjclHyU3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3N//AABEIAc4BsAMBIgACEQEDEQH/xAAcAAEBAAIDAQEAAAAAAAAAAAAAAQIFAwcIBAb/xABPEAACAQIDAwYKBwUEBwkBAAAAAQIDBQQGESExQQcSFlFWlBMUIjJSVWFxkdEVI0KBk6HBU2KSsdIINHLwM0NFY4Ky4SU1NlRkdKLC8ST/xAAXAQEBAQEAAAAAAAAAAAAAAAAAAQMC/8QAHBEBAQEBAAMBAQAAAAAAAAAAAAERAiExUUES/9oADAMBAAIRAxEAPwDvEAAAAAAAAAARoqAAAAAAAAAAAAAAAOOfnEh5yOTRBJICgAAAAAAAAAATUMgAo0KAAAAAAAAAAAAAAcdTziR845GkwkBQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACNjUm8CopNCgAAAAAAAAAAAZiZGOgAqKAAAAAAAAAAAAAAAAAAAAAAAAAAAAE1GpAMgAABxSflMsN4HIAAAAAAAAAAAAAEY1IBUEgigAAAAAAAAAAAAIgKAAAAAAAAAAAAAA45t6ki3rpqBygAAAAAAAAAAAABiNQBRoNCgARFAxcNXrqIx04mQAAAAAAAAAAE4AGQFQBIoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABhKOrEYaPVmYAAAAAAAAAAE1ApiNSoAkUAAAAIUAAAAAAAAAAAABiZESAaFAAAAAAAAAAAAADCU9GtDMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPcYmRACRQAAAAAAAAAAAAAAAAAI2UjCAoAAAAAAAAAAAEb0QFOOU9fNI23uMoRa37wEY8XvMwAAAAAAAAAAAABAiAoAAAAAAAAAAAADGUtHpoRS1emhJ+duJHXXXQDlAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEevAw8qXsOQAYxjotpkAAAAAAAAAAAAAAAAAAAAAAAAAAAIwKDFdbMgAAAAAAAAAAAAAAAAAJzkE09wFAAAAAAAAAAAAAATVdY1QFBABQQAUEAFBABQQAUE1GqAoIAKCFAAAAAAAAAEZSMA2QIy0AmhQAAAAAAAAAAAAAAAAAOKXnMtPeZOCZVFLcBQAAAAAAAAAAAAHFPzhDzjNx1Cik9QPwnKlZcwYm3O55YumOw+Jw6bq4WjVajWh1pekvzOiummadf/ENy93h2esjo7lk5PvFvC5ksmHfg5S52OoU15je+ol1el8es65v5Urr7ppmntBcvx2Ommae0Fy/HZoddVsBrkRvummae0Fy/HY6aZp7QXL8dmhAyDfdNM09oLl+Ox00zT2guX47NCBkG+6aZp7QXL8djppmntDc/wAdmhAyDfRzrmdPWV/uUlpu8Oy1M35ppta5huOrWr+vew0AQyDfLOmae0Ny/HY6aZp7QXL8dmhAyDfdNM09oLl+Oz6cNyh5xw22nmHGN/7xQqL/AOUWfmAP5iuzLRy2ZiwrjG54XB3CnxcYujP4rVfkjsPLvK5lm7yhRxVadtxEvs4vZBv/ABryfjoecB79xLxKmvZtOcakVOElKLWxxeqZmeVso57vuVakY4DEeGwaflYOvrKDX7vGL93wZ6ByTna05uwjngqngsXTS8NhKj+sh7favajO82Lr9QTUPcDlRshdCgAAAAAAAAAAAAAEYRQAAAAAAAAAAAAAAAAAAAAAAAAAMakYzhKM0pRktGmtUzIjWqA89cqvJzVsOIq3ey0Z1LVUk5VaUVq8K3v/AOD28Nx1rw2NP3Hs6UIzi4yScXsaa3nVmdORzBXKtUx2XK0cBiZbZYaUdaMn1rjF/l7DTnrPaY6EBvb9k7MWX5S+k7ViI04/6+lB1KT/AOKO779DQc+Hpx+J3sGQMefD04/xDnw9OPxKjIGPPh6cfiOfD04/xAZAx58PTj8Rz4enH4gZAx58PTj/ABDnw9OP8QGQMefF7pRfuZl8PiAAAA++2YzEWvF0bjgK9ShiaD50Zw3p/qvYfDBpTi2tUnuPoUl5VVc7Z9qW5+zQD0nycZ5w+brdzK3MpXOhFeHox3TXpx9j6uDP2aPH9mu+Lsl1w9xttV0q9CfOj1NcYvrTWxnqjKWYcLmaw4W6YPRRqrSdPjSmvOi/an+hj1zi63IAOVAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEZQAAAAE1QFBNSgAAAAAA+HE2e14qTnibdg60n9qpQjJ/Fo+1lA1fRyx+p7f3aHyJ0bsXqe392h8jagDVdG7F6nt/dofIdG7F6nt/dofI2pNQNX0bsXqe392h8h0bsXqe392h8jagDVdG7F6nt/dofIdG7F6nt/dofI2oA1UstWKS0dmt+n/ALaHyNFd+THKNzg1K008NN6vwuFbpyT+4/ZADz1nPkgudlpVMZZa8rnhIbZUnDSvBe5bJfdp7jrTrWm1bNp7PZ1Fyxcn1PFYavmOy0NMXS8vGUaa2Vo8Zpekt7617Tvnr6mOjS6vZt3EW1aoGqB2RyIZmdozJ9EYmemEubUYavZGsl5PxWz4HW5lSq1KFanXoycatKaqU5J7VJPVfmiWbB7OBrcuXSF6seAudPzcVQjU06m1t/M2Rg6AAAAAAGE209jJFvnbwOQAAAAAAAAAAAAAAAAAAAEAAAAAAAaTNljnfbPVwlDH4rAYheVRxGGrSpuM+GvNa1XWjdka1QHk+73HNlluNe33K73ejiaEubOLx9XR9TT521Pgz5Okl/8AX1279V/qPQfKfkSlm23KvhOZSu+Gi/F6j2KovQl7Hw6mebMRQq4bEVcPiKU6ValJxqU5rSUZLema82VK2HSS/wDr67d+q/1DpJf/AF9du/Vf6jVg6yI2nSS/+vrt36r/AFDpJf8A19du/Vf6jVgZBtOkl/8AX1279V/qDzJf3/t67L2ePVf6jVgZBtOkl/8AX1279V/qHSS/+vrt36r/AFGrAyDaxzPmGLTjmC7pr/11X+o3lq5T8326Udbq8XTX2MXTU/z2P8z8cBkHe+VuWy3Y2pHD5iwkrfUexYik3Oj9/GP5r2o7UoYmjiaMK2HqRq0prWM4PVSXsZ40T03H6rIWeLjlDG6U5VK9tqNeGwbls/xQ182X5PjwZxePi69Sp6lNfZLvgr5baNxtlZVcNWjzoy3Ndaa4NcUbAzUAAEYlFOLTSaa0aZQwPK/KXYY5czjjsFSio4eppiMOl6E+H3SUl9x+XO5P7RWDSq2THJLnNVKLfXukv5P4nTZtz6QAB0j0RyCY94vJEsNPfgsXUpLb9l6TX/O19x2QdNf2cqsnh7/RctYqpRml7WpJ/wAkdymF9ugAEAAAYSi2xGLT1MwAAAAAAAAAAAAAAACagUg4E0AqKAAAAAAAAABGk951fyvcn305Rle7LRX0nRj9dSitPGYL/wCy4de7qO0QJcHi/ds27OsHcvK/yewVStmKx0dFJuWNoQ3J8aqX/Np169Z0117ePA3l1AAFQAAAGLnBPRyin7x4SHpx+I0ZAx8JD04/EeEh6cfiTYrIGPhIenH4jwkPTj8RsR+05NM71sn3NwxDqVLViWvGKKevMf7SK6+vrXuR6WwWKo43D0sThasauHrQU6VSD1UovammeNPCQ9OPxOyuSTlEhl7Exs13xK+ia0vqqkpf3abf/I+PVv3GfUWPRIMISU0pRaaa1TW5mZwoAAOl/wC0ViY+DsmE+251Kv3JJfqdLn7blgvqvmdsSqMudhsDFYWk09ja2zf8T0/4UfiTbmeHIADod0/2coeRf56bOdQjr/GdznV/9n7Ayw+UMVi5xS8bxs3B9cYxjH+akdoGF9ugAEAAAAAAAAAAAACMCkBAMgRsAGQGQEKAAAAAAAAAAAI5aIA2kYOTexEScmzOEdN4GLppxamlJPemee+VvIDy7iXd7TTbtNef1lOK/u03w/wPh1buo9EHBi8LQxeGq4fFUo1qNWLjOnNaqSfAsuDxsD9jyk5Hr5QufOw6nUtOIk/F6r28z/dyfWuD4o/HG0uxyAAo7F5J832+14qNmv8Ah8PPA4ip9TiKlKMnQm+EnprzW/g3rsR34rPbGtfo7Cfgx+R4+69Vr7Gd2cjnKG66pZcvuI1rrycFiKj89fs5P0up8d3Dbn3P1Xav0Na/V2E/Bj8i/Q1s9XYT8GPyPtW4pmrXu02tf7Own4MfkFarY3/3bhPwY/I+ue9litpR8v0NbPV2E/Bj8ifQ1r9XYT8GPyPu1GpBjTpxpxjCCUYRSUYrcl1GZG9D4blebZaqDr3LH4XC0l9qtVUV7tu9+wD7m9Dr7lYz1Sy1ap4C31k7xioaU0tvgIvfUl+i4v3Ggznyz4alTqYXKkPD1ns8crwapw9sYvRt+/Re86WxeKxGOxdbF42vUxGJrS51SrUespP/AD+h3zz9HC2222223q23q2+sAGrkGkvsrWT2JdbB+25IstPMObaFWtDnYK3uOIra7pST8iP3yWvuRLcV37ky1KwZWtlt5uk6NCPhNmnlPbL82buMtXoYSW39RHztTBXKAAAAAAAAAAABNQBAVIAigAYgGSAiKAAAAAAAAAAAABnGot66nIAIloUAAAANdfbPgb7a69tuVFVcNWWko8U+DT4NPbqeX86ZWxmU7zPAYpSqUZeVhq+mirQ+a3NfNHrE0Ocsr4HNlmqW/HR5svOo1oryqU+DX8n1o656xHk0GxzBZsbYLviLXc6fMxFF7+E48JRfFP5rga42QLGUotShJxlFpqUXo01uaIAPQ3JLygdIsGrXdqsVdsPHyZPZ4zBfaX73X8Tso8a4PFYjA4uji8HWlRxFGanTqQejjJcT0xybZ3oZwtmtRwpXLDpLE0U9Neqcf3X+Rj1zit9mSy0r9aK2Aq1q+Hc1rCth5uE6cuDTX8uJ5kzJRzJlq7Vbbc7jcI1YbYzWKqc2rDhKO3d/I9Xn5TlCyZhM4WnwMubSx1DWWFxGnmN74vri+K+8kuK8z/TF19a3DvdT5j6YuvrW4d7qfM4rjgcVbMdXwOPozoYqhLm1Kc1tT+T3p8UfObZHL7fpe6vfdbh3qp8z45ylUqOpUlKdR75zblJ/eyAZAABQALCLnJQhFylJ6RjFatvgkBnhqFbFYilh8NTlVr1pqnSpxW2cm9iXvPUfJ3lSllHL9PBrysXVfhcXU9Ko1povYlol/wBT8tyScncrDTV6vVL/ALUqR+poy2+LRfX+++PVuO0jLrrVgADhQAAAAAAAAagxArZAZATQoAAAAAAAAAAAAAAABi5JFUk9wFAAAAAAAAAAAAAfj+UfJFDONp5sObSuWHTlha7WzXjCX7r/ACe32HmXG4TEW/F1sHjaUqOJoTcKlOW+MkeyjrjlYyBHMuDd0tlNRu+Hh5sV/eYL7L/eXB/cd89YjzsCyjKEnGcZRlF6SjJaNPimiGqB99ivOOsN1oXK21fB4mi+K1U1xjJcUz4AL5HrHJmacFmuzQuGDfMmvJr0G9ZUZ8Yv9HxN9rqeTMnZpxuUrxG4YFKpBrm18PKWka0OrXg+p8D1HY7zgr5a6FxttZVcPXWsXxXWmuDXEx65xZX5DlVyFDNOB8ft8YwvGGi/Bvcq8fQft6n8zzhOE6c5QqQlCcJOMoTWji1saa4NHs7TXadScsXJ87jCpmCyUdcbBa4uhFba8F9pfvL817tt56z2OiwOG8GqAByU6M5aPRqPXoB9dls1xvuOhgrThKmJry3qC8mC65S3JHfvJ3yY4PLLhcLlKGMuu+MtPIof4E97/efwR1HkbONfJl0i0pVsBXaWLoJ716a9qXD2ae1elrbjcPcMDQxmDrRrYevFTp1IPVSTM+7Vj6UtFoUAzUAAAAAAAAAAAmhQAAAAAAAAAAAAAAAAAAAHE95ae9mfNQUUtwFAAAAAAAAAAAAARhbigDp7lk5PvGY1syWSjrXgudjcPTW2pFb6iXpLj1o6Q11Wq/I9nSS4o6E5YOT52mtUv9mor6PqPXE0YL/QTb85L0Xr9z9m7vnr8Sx1YADVA/X8m+da2T7q3VUqtrxDXjNGO1xfpxXWuPWvcj8gCWaensjB4uhjsLSxWDrQrYetFTp1IPVSi9zRz6abeJ525J+UB5bxUbVdqsnaa8vJnJ/3ab4/4Xx6t/WeiYyjKKcWmmtU096MbMdOieWLk+WAnVzHZaOmEnJyxlCC2UpP7aXot7+redTLbpoezatOFSnKE4RlCS0lGS1TXUzztym5Cjli4O44KLdnrzfNjpqsPJ/Yfs9H4dR3x1+JX4OGH3OcoqL1e/bs3mVerCMUqbWu73JrTYYYmpr5MJax46M4NTRDX8zsHkpz9PK+OVuuU3Kz4me1t/3ab+0v3XxX3nXwJZo9m0pxqU4zpyUoSWsZRexozOieRvlA8QnSy7eqz8VnLm4KvN7KTf8Aq2/RfDq3btDvYxsyugAEAAAAABGEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACcSmLAqKRIoAAADixNCniaNShXpQq0akXGcJrWMk96a4o5QB5p5UMiVMpXDxnAwlOz4mX1Ut/gJP7EvZ1P/L/CnsS7WzCXe34jAXCjGthq8HCcJcV+j9p5fz5k/GZPu7w1bnVcHV1eExLX+kj1S/eXFfea8dalfmwAdoPcdxcjXKD4GdHLd8rfVvycDiJvzX+zk/5P7jp0b9+u/gSzR7P1TPmx+Aw1xwdbB42jCthq8XGpTmtVJM625IOUH6Yw9OxXqs3cqMNKFab/ALzBdb9Nfnv36naSeqML4rp5a5Q8mYrJ14dNp1LfXbeExG/VehL95fnvPyp67zJYcDmS01rbc6SnRqLVSXnU5LdKL4NHl3NmW8dlW81bbj466PnUayXk1ocJL9VwZrz1qNMADtEa1R3vyPcoX0lTpZfvdd+P01pha9R7cRBLzW/TX5r3HRJlTqVKVSFSlOUKkJKUJxeji1tTT6znqaPZpTr7krz/AE804DxPHyULvh4rwi4V4+nH9Vw+87AT1MbMdKTUpiBddpTFGQAAAAAAAAAAAAAAAAAE4lAAAAAAAAAAAAAABGyNgChIoAAAAAAAAAGozTl7AZms9a23KnrTmtYzj51OXCS9qNuAPJmb8p3PKdxeFuVLWlJvwGJgvIrrrXU/ZvNEew7va8BeMDUwVzwlPFYap51OotV/0ftR07mnkTqxlOvlfGKcd/iuLlo17FNfr8TSd/UdPA3d0yhmO0yauNkx1LT7UaXhIv3ShqvzNS8NiE9HhsQn1OlL5HexEw+IrYavTr4erOlWpSU4VIPRxa3NHpHkvz5SzZbvFsXKNO74WC8PT3Kqt3hI+x8Vwf3Hm/xev/5ev+HI+u04u52a40LhbY4ijiqMtYSVKTXtTXFPqJ1JR7APzed8o4HN9olg8Z9XXh5WHxEV5VKf6p8UMj5qoZrs0MXCnLD4qHk4nDz11pz9nXF8GfpDH06eQ8xZfuWW7lPAXag6dSL8ia8yrHhKL4r+XE1Z69v9gteYcBLBXfCQxFF7VrslB9cZLan7jprM/IpccNKpWy3io4ylt0w2IkoVF7FLc/v095pO/qY6nBtrnli/2qbjcLNjqOj05zoOUX7pR1TNc8NXW/DV/wAKXyO9iM7djsXbMdQx1vxE8PiqElOnVjvTXWuK61xR6dyDnPCZvtCr09KeOpJLFYbjB9a/dej0f3b0eY6WEr85OdCpzWtnkNG1y1eLrl660bjbqNSFaitJxnGXNqw12xa9v5bydSVXrCO1F0NPlfMGEzJaKVwwPPipLSpSqLSdKfGMl/nU3JioAAAAAAAAAAAOObeu8Qb5wHIAAAAABAAAAAAAAAAACagGQuuoQE01MgAAAAAAAAAAAAAAAAABxujTb1lTg31uJyADj8BS/ZQ/hQ8BS/ZQ/hRyADCNKEW3GEYt9S0MwAABNQIzDwNN7XTh/CjNhIDDwNL9lT/hRfAUv2UP4UcgAwjCMNeZGMdd+i0MwAAAAAAAAAAAAwlHV7BGLTMwAAAERQAAAAAAAAAAAAGJkTQBoUAAAAAAAAAAAAAAAAAAAAAAAAAAAAIyGQ0AiRQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMHPRljLVmEk23sLBaMDkAAAAAAAAAAAAAYuSWwKab00MZ+cILykByAAAAAAAAAAAAAAIwgKAAAAAAAAAAAAAAEQFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIwKDFGQDQaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAE5yCaYFAAAAAAAAAAAAAADFgXULaEVATQoAAAAAAAAAAE1Q1QFAAAAAAAAAAAAAACagUAAAAAAAAAAAAAAAAAAAABwy3syhvMnFNlUUtwFAAAAAAAAAAAjDIABUUAAAAAAAAAAAAAA4pecIedoZuKYUUnqBkAAAAAAAAAAAAA45+cIecZOKe8RikBkAAAAAAAAAAAAAj3BFAAAAAAAAAAAAAAAAAAhTEAVIaFAAAAAAAAAAAAAAAAAAAAAAAAAAAARspiBVvKRFAAAAAAAAAAAAAAAAAAAAAAAIAKCACggAoIAKCACk0AAoIAKCACggAoIAKDCTaewkZNyXyA5AQAUEAFBABQQAUEAFIwAIVBACggAoIAKCACggAoIAKCACggAoIAKCADy/0tzL2guXeZDpdmXtBcu8yNKcuFhCpi6FOq6ipTqxjN04uUlFtJ6Jb3projTI4bTpfmTVLpDctXu/8A6ZfMvS7MvaC5d5kftK1is9ptV2qTt9Wtba6wkaco4xuc9cQ4ppygnBrVax09mp8OMyZbcDcZQnHFVsLRwWMrznz9E50a/NitdNFrFbV95PC4/M9Lsy9oLl3mQ6XZl7QXLvMj9/drFZcdicVgYWXEYaVS+U8O8RTaSpxlT1U0lHZT4Jbudt3mnu2V8v27DYnHOOKqU6FOnF4WliHLSU63g4y57gm+vm6b9g8GV+Y6XZk7QXLvMh0tzL6/uXeZH7OnlyzYHx2tRtNbG4aVqxMqM/GW6k5R5mq5koJ06nNls0T04M09uyxbr3d8JTtFDFu10Wo43F1J6eFm4c7m000tGpJxe/rHgaPpfmTXTpBcterxmXzL0uzL2gufeZH6602K23DL+PcLXccHSjUp4lYGvKPjFdqnN8ynJpbHp7/Jls3HXmInRnNOhRdJc1axc+dt4vUTBteluZe0Fy7zIdLcy9oLl3mRpQXEbrpbmXtBcu8yHS3MvaC5d5kaUDBuuluZe0Fy7zIdLcy9oLl3mRpQMG66W5l7QXLvMh0tzL2guXeZGlAwbrpbmXtBcu8yHS3MvaC5d5kaUDBuuluZPX9y7zIdLsy9oLl3mRpQMG66W5l7QXLvMh0tzL2guXeZGlAwbrpbmXtBcu8yHS3MvaC5d5kaUDBuuluZe0Fy7zIdLcy9oLl3mRpQMG66W5l7QXLvMh0tzL2guXeZGlAwbrpbmXtBcu8yHS7MvaC595kaUDBuul2Ze0Fy7zIdLsy9oLl3mRpQMNbrpbmXtBcu8yHS3MvaC5d5kaUDBuuluZe0Fy7zIdLcy9oLl3mRpQMG66W5l7QXLvMh0tzL2guXeZGlAwbrpbmXtBcu8yHS3MvaC5d5kaUDBuuluZe0Fy7zIdLcy9oLl3mRpQMG66W5l7QXLvMh0uzJ6/ufeZGlPvtbtqVd3N1diXg1S3vyZ6+zfzN/8tRg+vpbmXtBcu8yHS3MvaC5d5kclSOWvK8DXqxlzppOpTqc3muTa00Wuqjotui119mvFN2P6QxEoTfiyh9Q5Kro5c2fneTzvO5nD47Qq9Lcy+v7l3mQ6XZl7QXLvMj5JxtvjGJ8HU+o5j8Dz1PVy4bo8Pb7uOz6oKyKpLwtSXM8J5LgqnmaLg179Pz3BF6XZl7QXLvMidLsydoLl3mXzOOSs6qQcatTwXPhztIz52mj5y2pLfzePXproYL6L0km5qaguZLmy5rlz+PFeTpuT4jB9HS7MvaC5d5kOluZe0Fy7zI1eL8D4zU8VetHXyG9d33nCMAyp1J0qkalKcoTg1KMovRprc01uMQUfXibpccXzni7jja7koqTq4ic+dzXrHXV8HtXUzKd2uc6FXDyuWNlQrNyq0niZuFRve5LXRtnxAD63dLi+cncMY1KMYvXET2xi9YrfuT3dRcTdLjipSlirjja0p83nOriJyb5r1jrq+D2rqZ8YA+2rd7pVr08RUumPnXpJqnVniZucE96UtdUcdO4Y2lShSpY3FQpwqeFhCFaSjGfpJa7Je3efMAPsq3a5Vaqq1bljZ1VNT8JPEzcuctz1111XBnySlKcpSnJylJ6yk3q2+tkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH04Sng6kZvF4mVFx81RpuXO+C2HPKhbNVzLhNrwiT51GS8ni933aez3HwakA2Lo2rnt+O1Obo9I+Clrrt483dsXxK8PaFU2Y+s4b/8ARPb7NxrRx1A2DoWxKWmNm/Jen1cl5XBeb/8AntJToW1xi6uOmti15tOT9/2f86e3Z8AAzqqCm1Sk5Q4NrRmAAH//2Q==";
+
+function b64url(x) {
+  const b = Buffer.isBuffer(x) ? x : Buffer.from(x);
+  return b.toString("base64").replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/,"");
+}
+function pdfText(s) {
+  return String(s ?? "").replace(/\\/g,"\\\\").replace(/\(/g,"\\(").replace(/\)/g,"\\)");
+}
+function money(n) { return "$" + Number(n || 0).toFixed(2); }
+function wrap(text, max) {
+  const words = String(text || "").trim().split(/\s+/).filter(Boolean);
+  const lines = []; let line = "";
+  for (const word of words) {
+    const next = line ? line + " " + word : word;
+    if (next.length > max && line) { lines.push(line); line = word; }
+    else line = next;
+  }
+  if (line) lines.push(line);
+  return lines.length ? lines : [""];
+}
+function jpegInfo(buf) {
+  let i = 2;
+  while (i < buf.length) {
+    if (buf[i] !== 0xFF) { i++; continue; }
+    while (buf[i] === 0xFF) i++;
+    const marker = buf[i++];
+    const len = buf.readUInt16BE(i);
+    if ([0xC0,0xC1,0xC2,0xC3,0xC5,0xC6,0xC7,0xC9,0xCA,0xCB,0xCD,0xCE,0xCF].includes(marker)) {
+      return {height:buf.readUInt16BE(i+3),width:buf.readUInt16BE(i+5),components:buf[i+7]};
+    }
+    i += len;
+  }
+  throw new Error("Could not read logo.");
+}
+function makePdf(i) {
+  const logo = Buffer.from(LOGO_JPEG_BASE64, "base64");
+  const img = jpegInfo(logo);
+  const colorSpace = img.components === 1 ? "/DeviceGray" : "/DeviceRGB";
+  const T=(x,y,size,s,bold=false)=>`BT /${bold?"F2":"F1"} ${size} Tf 1 0 0 1 ${x} ${y} Tm (${pdfText(s)}) Tj ET\n`;
+  let c="q\n1 1 1 rg 0 0 612 792 re f\n";
+
+  c+="q 1 0 0 1 42 676 cm 88 0 0 88 0 0 cm /Im1 Do Q\n";
+  c+="0.04 0.08 0.14 rg\n"+T(145,742,27,"RANKORA",true)+T(146,719,10,"I N V O I C E");
+  c+="0.75 0.77 0.80 RG 0.8 w 320 690 m 320 762 l S\n";
+  c+=T(372,742,29,"INVOICE",true)+T(373,718,10,"INVOICE #:",true)+T(470,718,10,i.invoiceNumber||"—");
+  c+=T(373,696,10,"DUE DATE:",true)+T(470,696,10,i.dueDate||"Not selected");
+  c+="0.75 0.77 0.80 RG 0.8 w 42 668 m 570 668 l S\n";
+
+  c+="0.04 0.08 0.14 rg\n"+T(42,635,12,"BILLED TO",true)+T(42,603,13,i.customerName||"Customer Name",true);
+  let y=581;
+  for(const line of [...wrap(i.customerAddress,38),...wrap(i.customerEmail,38),...wrap(i.customerPhone,38)]){c+=T(42,y,10,line);y-=16;}
+  c+=T(330,635,12,"FROM",true)+T(330,603,13,"Rankora",true)+T(330,581,10,"386-868-9183")+T(330,565,10,"rankorainvoices@gmail.com");
+
+  const top=515;
+  c+="0.04 0.08 0.14 rg 42 "+top+" 528 30 re f\n1 1 1 rg\n";
+  c+=T(56,526,10,"DESCRIPTION",true)+T(305,526,10,"QTY",true)+T(390,526,10,"UNIT PRICE",true)+T(495,526,10,"TOTAL",true);
+  const productLines=wrap(i.product||"Product / Service",38), rowHeight=Math.max(48,30+productLines.length*15), bottom=top-rowHeight;
+  c+="1 1 1 rg 42 "+bottom+" 528 "+rowHeight+" re f\n0.75 0.77 0.80 RG 0.7 w 42 "+bottom+" m 570 "+bottom+" l S\n0.04 0.08 0.14 rg\n";
+  let py=top-24; for(const line of productLines){c+=T(56,py,11,line);py-=15;}
+  c+=T(315,top-24,11,String(i.quantity||1))+T(390,top-24,11,money(i.unitPrice))+T(495,top-24,11,money(i.total));
+
+  const nt=bottom-46;
+  c+=T(42,nt,11,"NOTES",true)+"0.04 0.08 0.14 RG 1 w 42 "+(nt-10)+" m 75 "+(nt-10)+" l S\n";
+  let ny=nt-34; for(const line of wrap(i.notes||"Thank you for your business! If you have any questions, please don't hesitate to contact us.",44).slice(0,4)){c+=T(42,ny,10,line);ny-=15;}
+  c+=T(42,ny-20,18,"Thank You!");
+
+  const tx=330, tt=bottom-44;
+  c+=T(tx,tt,11,"SUBTOTAL",true)+T(500,tt,11,money(i.total));
+  c+=T(tx,tt-28,11,"TAX (0%)",true)+T(500,tt-28,11,"$0.00");
+  c+="0.75 0.77 0.80 RG 0.8 w "+tx+" "+(tt-45)+" m 570 "+(tt-45)+" l S\n";
+  c+="0.04 0.08 0.14 rg "+tx+" "+(tt-87)+" 240 36 re f\n1 1 1 rg\n"+T(tx+12,tt-64,12,"AMOUNT DUE",true)+T(485,tt-64,14,money(i.total),true);
+
+  c+="0.04 0.08 0.14 rg 0 0 612 86 re f\n";
+  c+="q 1 0 0 1 30 18 cm 45 0 0 45 0 0 cm /Im1 Do Q\n1 1 1 rg\n";
+  c+=T(95,52,12,"RANKORA",true)+T(95,34,8,"386-868-9183  •  rankorainvoices@gmail.com")+T(385,40,11,"We appreciate your business!");
+  c+="Q\n";
+
+  const stream=Buffer.from(c);
+  const objs=[
+    Buffer.from("<< /Type /Catalog /Pages 2 0 R >>"),
+    Buffer.from("<< /Type /Pages /Kids [3 0 R] /Count 1 >>"),
+    Buffer.from("<< /Type /Page /Parent 2 0 R /MediaBox [0 0 612 792] /Resources << /Font << /F1 4 0 R /F2 5 0 R >> /XObject << /Im1 6 0 R >> >> /Contents 7 0 R >>"),
+    Buffer.from("<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>"),
+    Buffer.from("<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica-Bold >>"),
+    Buffer.concat([Buffer.from(`<< /Type /XObject /Subtype /Image /Width ${img.width} /Height ${img.height} /ColorSpace ${colorSpace} /BitsPerComponent 8 /Filter /DCTDecode /Length ${logo.length} >>\nstream\n`),logo,Buffer.from("\nendstream")]),
+    Buffer.concat([Buffer.from(`<< /Length ${stream.length} >>\nstream\n`),stream,Buffer.from("endstream")])
+  ];
+  let out=Buffer.from("%PDF-1.4\n"),offs=[0];
+  objs.forEach((o,n)=>{offs.push(out.length);out=Buffer.concat([out,Buffer.from(`${n+1} 0 obj\n`),o,Buffer.from("\nendobj\n")]);});
+  const xref=out.length; let x=`xref\n0 ${objs.length+1}\n0000000000 65535 f \n`;
+  for(let n=1;n<offs.length;n++)x+=`${String(offs[n]).padStart(10,"0")} 00000 n \n`;
+  x+=`trailer\n<< /Size ${objs.length+1} /Root 1 0 R >>\nstartxref\n${xref}\n%%EOF`;
+  return Buffer.concat([out,Buffer.from(x)]);
+}
+
+export default async function handler(req,res){
+  if(req.method==="GET")return res.status(200).json({ok:true,message:"Rankora PDF invoice API is working."});
+  if(req.method!=="POST")return res.status(405).json({error:"Method not allowed."});
+  const i=req.body||{};
+  if(!process.env.GOOGLE_CLIENT_ID||!process.env.GOOGLE_CLIENT_SECRET||!process.env.GOOGLE_REFRESH_TOKEN)
+    return res.status(500).json({error:"Missing Gmail environment variables in Vercel."});
+  try{
+    const form=new URLSearchParams({
+      client_id:process.env.GOOGLE_CLIENT_ID,
+      client_secret:process.env.GOOGLE_CLIENT_SECRET,
+      refresh_token:process.env.GOOGLE_REFRESH_TOKEN,
+      grant_type:"refresh_token"
+    });
+    const tr=await fetch("https://oauth2.googleapis.com/token",{method:"POST",headers:{"Content-Type":"application/x-www-form-urlencoded"},body:form});
+    const td=await tr.json();
+    if(!tr.ok)throw new Error(td.error_description||td.error||"Could not refresh Gmail authorization.");
+
+    const filename=`Rankora-Invoice-${String(i.invoiceNumber||"invoice").replace(/[^a-zA-Z0-9_-]/g,"-")}.pdf`;
+    const boundary="rankora_"+Date.now().toString(36);
+    const raw=`To: ${i.customerEmail}\r
+From: Rankora <rankorainvoices@gmail.com>\r
+Subject: Rankora Invoice #${i.invoiceNumber}\r
+MIME-Version: 1.0\r
+Content-Type: multipart/mixed; boundary="${boundary}"\r
+\r
+--${boundary}\r
+Content-Type: text/plain; charset=UTF-8\r
+\r
+Hello ${i.customerName||""}, your Rankora invoice is attached as a PDF.\r
+\r
+--${boundary}\r
+Content-Type: application/pdf; name="${filename}"\r
+Content-Disposition: attachment; filename="${filename}"\r
+Content-Transfer-Encoding: base64\r
+\r
+${makePdf(i).toString("base64")}\r
+--${boundary}--`;
+
+    const sr=await fetch("https://gmail.googleapis.com/gmail/v1/users/me/messages/send",{
+      method:"POST",
+      headers:{Authorization:`Bearer ${td.access_token}`,"Content-Type":"application/json"},
+      body:JSON.stringify({raw:b64url(raw)})
+    });
+    const sd=await sr.json();
+    if(!sr.ok)throw new Error(sd.error?.message||"Gmail could not send the invoice.");
+    return res.status(200).json({success:true,id:sd.id});
+  }catch(e){return res.status(500).json({error:e.message||"Server error."})}
+}
