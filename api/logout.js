767a4e96c270122c41e37d1b5845d2034fdefab7
noahhyy <126304197+noahhyy@users.noreply.github.com>
2026-09-25 21:12:44 -0400

    Change export to CommonJS and clear user session

--- api/logout.js
+++ api/logout.js
@@ -1,10 +1,10 @@
-export default function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader(
     "Set-Cookie",
-    "biofyit_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0"
+    "biofyit_user=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0"
   );
 
   return res.status(200).json({
     success: true
   });
-}
+};
